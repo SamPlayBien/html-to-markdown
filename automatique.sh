8bb8109ba29cbe4e6b6dcca868c71e54e2f29c19
SamPlayBien <93989462+SamPlayBien@users.noreply.github.com>
2021-11-11 15:30:43 +0100

    Update automatique.sh

--- automatique.sh
+++ automatique.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+#Vide du .env
 > .env
 
+#Demande de l'URL
 echo "Rentrer l'URL de la page HTML à convertir en Markdown :"
 read URL
 
+#Demande du port
 echo "Rentrer le port à ouvrir pour afficher l'éditeur :"
 read port
 
+#URL dans le .env
 echo "URL=$URL" >> .env
 
+#Port dans le .env
 echo "port=$port" >> .env
 
+#On clear le docker compose et on le up/build
 docker-compose down && docker-compose up --build &
 
+#Ouverture de la page de l'editeur
 xdg-open http://127.0.0.1:$port/
